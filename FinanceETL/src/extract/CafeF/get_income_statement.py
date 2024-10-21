7ac406e7216e818c0fef37fcf5b413f84f645329
HungphamLeo <134383049+HungphamLeo@users.noreply.github.com>
2024-10-21 19:45:00 +0700

    update

--- FinanceETL/src/extract/CafeF/get_income_statement.py
+++ FinanceETL/src/extract/CafeF/get_income_statement.py
@@ -16,7 +16,7 @@ class IncomeStatementCrawler(BaseCrawler):
         if year is None:
             year = self.year
     
-        column_names = self.generate_column_quarter_names(number_quarter=number_quarter, year=year, quarter=quarter, name_of_tables=f"{company_name}_Income_Statement_Quarter") 
+        column_names = self.generate_column_quarter_names(number_quarter=number_quarter, year=year, quarter=quarter, name_of_tables=f"Income_Statement_Quarter") 
         round_merge = number_quarter // 4
         final_table = None
     
@@ -38,16 +38,17 @@ class IncomeStatementCrawler(BaseCrawler):
             year = self.year
         round_merge = int(number_year // CONST_TABLE)
         final_table = None
-    
+        column_names = self.generate_column_year_names(number_year=number_year, year=year, name_of_tables=f"Income_Statement_Annual") 
         for _ in range(round_merge):
-            current_year = year - CONST_TABLE
-            __table = self.extract_table_content(generate_income_statement_url(company_name, str(current_year), "1 YEAR", str(0)), 4)
+            
+            __table = self.extract_table_content(generate_income_statement_url(company_name, str(year), "1 YEAR", str(0)), 4)
             __table = __table.iloc[:, :-1]
             if final_table is None:
                 final_table = __table
             else:
                 final_table = pd.concat([__table,final_table.iloc[:,1:]], axis=1)
-        column_names = self.generate_column_year_names(number_year=number_year, year=year, name_of_tables=f"{company_name}_Income_Statement_Annual") 
+            year = year - CONST_TABLE
+        
         final_table.columns = column_names  
         return final_table
 
--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-This folder is just a small example about a system which get data from multisourcing to modify with the variety of purposes.
-- trading data provider: Use XTB exchange is a example for listen message, build socket and process real-time message, use api to place order, get order history,  get news, etc
-- research data provider: Use Alpha Vantage is a example to get historical data of multiassets to build models, get data for research, describe and analysis
+Finance ETL include:
+ - extract data from multisourcing: Alpha_Valtage, Web CafeF, XTb
+    Alpha_Valtage: get data from this platform, build function to get economics, stocks data, price ,... and format the common structure
+    CafeF: get data by craw web data html. We focus to get finance statement annual or quarter of vietnam stocks: income statement, balance sheet, cash flow.
+    => transform to final data of XTB and CafeF same.
+    XTB: connect with platform with socket, api to get data for trading and save the real-time data on Redis for easily to get
 
+Data lake: HDFS => Save raw data 
+Load_db: Save the data proccessed to MySql and Mongo
