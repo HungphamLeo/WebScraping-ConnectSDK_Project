8df96165159d6082ad608a69176a31bbdd1a349e
HungphamLeo <134383049+HungphamLeo@users.noreply.github.com>
2024-10-27 22:01:15 +0700

    update 1

---
+++ FinanceETL/src/extract/CafeF/config.py
@@ -0,0 +1,5 @@
+BANK_LIST = ["acb","bab","bid","ctg","eib","hdb",
+            "mbb","msb","nvb","ocb","shb","ssb",
+            "stb","tcb","tpb","vcb","vib","vpb"]
+RENEWABLE_ENERGY_LIST  = ["asm","bcg","fcn","hdg", "lcg", 
+                          "ree","tv2","geg","pc1","tta"]
